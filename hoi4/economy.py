952d0eabdbab195ef50877062ef18e8e8e453181
DanZanzibar <121912193+DanZanzibar@users.noreply.github.com>
2023-02-18 16:16:01 -0700

    Added Mils object

--- hoi4/economy.py
+++ hoi4/economy.py
@@ -13,22 +13,33 @@ research = {
 }
 
 class Economy:
-
-    def __init__(self, Production, Construction, Naval_Production) -> None:
-        self.prod = Production
-        self.cons = Construction
-        self.navp = Naval_Production
-
-    def change_date(self):
-        pass
-
-
-class Production(Economy):
-    pass
-
-class Construction(Economy):
-    pass
-
-class Naval_Production(Economy):
-    pass
-
+		
+		def __init__(self, date, mils, civs, dyards, resources, fact_out, prod_cap):
+				self.date = date
+				self.resources = resources
+				self.fact_out = fact_out
+				self.prod_cap = prod_cap
+				
+		def advance_date(self, new_date):
+				self.date = new_date
+				
+		def daily_prod(self):
+				for mil in mils:
+						
+		
+		
+class Mil(Economy):
+		
+		def __init__(self, equipment, prod_eff):
+				self.equip = equipment
+				self.prod_eff = prod_eff
+			
+		def daily_prod(self):
+				output_IC = Economy.fact_out * self.prod_eff * mil_base
+				equip_cost = self.equip.get_IC()
+				output_equip = output_IC / equip_cost
+				self.prod_eff_daily_gain()
+				return self.equip, output_equip
+			
+		def prod_eff_daily_gain(self):
+				pass
