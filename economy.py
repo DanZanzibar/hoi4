51632fa53a8ffce7d0420db7c6a26d286dd674c8
Zan Owsley <zanowsley@gmail.com>
2023-02-23 21:44:34 -0700

    now just 2 mils to start

--- economy.py
+++ economy.py
@@ -1,6 +1,5 @@
 import forces
 from datetime import date, timedelta
-from copy import deepcopy
 
 one_day_inc = timedelta(days=1)
 mil_base = 4.5
@@ -9,7 +8,7 @@ dyd_base = 2.5
 
 class Mil:
 		
-	def __init__(self, init_date, prod_eff, prod_cap, fact_out):
+	def __init__(self, init_date: date, prod_eff: float, prod_cap: float, fact_out: float):
 		self.init_date = init_date
 		self.init_eff = prod_eff
 		self.prod_eff = prod_eff
@@ -33,7 +32,7 @@ class Mil:
 
 class Dyd:
 
-	def __init__(self, init_date, dyd_out) -> None:
+	def __init__(self, init_date: date, dyd_out: float) -> None:
 		self.init_date = init_date
 		self.dyd_out = dyd_out
 		self.produced = 0
@@ -45,35 +44,25 @@ class Dyd:
 
 class Production:
 		
-	def __init__(self, date, mils, dyds, bonus_sched, cons_sched, base_prod=0.1, prod_cap=0.5, fact_out=1, dyd_out=1):
+	def __init__(self, date: date, bonus_sched: dict, base_prod=0.1, prod_cap=0.5, fact_out=1, dyd_out=1):
 		self.date = date
 		self.bonus_sched = bonus_sched
-		self.cons_sched = cons_sched
 		self.base_prod = base_prod
 		self.prod_cap = prod_cap
 		self.fact_out = fact_out
-		self.mils = []
-		for entry in mils:
-			if isinstance(entry, int):
-				self.new_mils(entry)
-			else:
-				self.new_mils(*entry)
+		self.mils = [Mil(date, prod_cap, prod_cap, fact_out), Mil(date, base_prod, prod_cap, fact_out)]
 		self.dyd_out = dyd_out
-		self.dyds = []
-		self.new_dyds(dyds)
-		self.start = deepcopy(self)
+		self.dyds = [Dyd(date, dyd_out)]
 
-	def new_mils(self, num_of_mils, curr_prod_eff=None):
+	def new_mil(self, curr_prod_eff=None):
 		if curr_prod_eff:
 			prod_eff = self.prod_cap if curr_prod_eff == 'max' else curr_prod_eff
 		else:
 			prod_eff = self.base_prod
-		for x in range(num_of_mils):
-			self.mils.append(Mil(self.date, prod_eff, self.prod_cap, self.fact_out))
+		self.mils.append(Mil(self.date, prod_eff, self.prod_cap, self.fact_out))
 
-	def new_dyds(self, num_of_dyds):
-		for x in range(num_of_dyds):
-			self.dyds.append(Dyd(self.date, self.dyd_out))
+	def new_dyd(self):
+		self.dyds.append(Dyd(self.date, self.dyd_out))
 
 	def update_mods(self):
 		if self.date in self.bonus_sched:
@@ -94,25 +83,17 @@ class Production:
 					self.dyd_out += bonus
 					for dyd in self.dyds:
 						dyd.dyd_out = self.dyd_out
-
-	def update_cons(self):
-		if self.date in self.cons_sched:
-			mils, dyds = self.cons_sched[self.date]
-			if mils:
-				self.new_mils(mils)
-			if dyds:
-				self.new_dyds(dyds)
 			
 	def advance_day(self):
+		if self.date.day == 1: self.new_mil(self.base_prod)
 		self.update_mods()
-		self.update_cons()
 		for mil in self.mils:
 			mil.daily_prod()
 		for dyd in self.dyds:
 			dyd.daily_prod()
 		self.date += one_day_inc
 
-	def advance_to_date(self, new_date):
+	def advance_to_date(self, new_date: date):
 		num_days = (new_date - self.date).days
-		for day in range(num_days):
+		for _ in range(num_days):
 			self.advance_day()
