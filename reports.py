8471af32256de46a4235745c38510ea18c97f598
DanZanzibar <121912193+DanZanzibar@users.noreply.github.com>
2023-02-28 10:46:49 -0700

    not much to say

--- reports.py
+++ reports.py
@@ -1,18 +1,28 @@
 import forces, economy
 from datetime import date, timedelta
-
-
-def get_mil_IC(prod: economy.Production, init_date, start_maxed=False):
-	for mil in prod.mils:
-		if mil.init_date == init_date and mil.start_maxed == start_maxed:
-			return mil.produced
-	raise Exception('No such Mil')
-			
-
-def get_dyd_IC(prod: economy.Production, init_date):
-	for dyd in prod.dyds:
-		if dyd.init_date == init_date:
-			return dyd.produced
-	raise Exception('No such Dyd')
+	
+	
+def mils_by_date(prod_obj):
+	mils_prod = {}
+	for mil in prod_obj.mils:
+		if 'maxed_at_start' in mils_prod:
+			mils_prod[mil.init_date] = mil.produced
+		else:
+			mils_prod['maxed_at_start'] = mil.produced
+	return mils_prod
+	
+	
+def dyds_by_date(prod):
+	dyds_prod = {}
+	for dyd in prod_obj.dyds:
+		dyds_prod[dyd.init_date] = dyd.produced
+	return dyds_prod
+	
 	
 
+	
+	
+def add_all_production(prod, *mils):
+	mils_dict = mils_by_date(prod)
+	date, quantity = mil
+	total += quantity * mils_dict[date]
